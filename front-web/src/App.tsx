384d09d6007fc1d78e43f8c9b58abc748bf00274
matheusned15 <matheusned15@gmail.com>
2021-01-08 20:32:17 -0300

    Finish front-web

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,9 +1,14 @@
+import {ToastContainer} from 'react-toastify'
 import './App.css';
+import 'react-toastify/dist/ReactToastify.css';
 import Routes from './Routes';
 function App() {
   return (
+    <>
     <Routes />
-  );
+    <ToastContainer />
+    </>
+    );
 }
 
 export default App;
