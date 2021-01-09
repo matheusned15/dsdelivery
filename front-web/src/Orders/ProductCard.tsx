392ae3519250948e097045cb4807febcd3dcbcd9
matheusned15 <matheusned15@gmail.com>
2021-01-08 21:40:13 -0300

    Last commit

--- front-web/src/Orders/ProductCard.tsx
+++ front-web/src/Orders/ProductCard.tsx
@@ -18,7 +18,10 @@ function ProductCard({ product, onSelectProduct, isSelected }: Props) {
             <h3 className="order-card-title">
                 {product.name}
             </h3>
-            <img src={product.imageUri} className="order-card-image" />
+            <img
+                src={product.imageUri} className="order-card-image"
+                alt={product.name}
+            />
 
             <h3 className="order-card-price">
                 {formatPrice(product.price)}
